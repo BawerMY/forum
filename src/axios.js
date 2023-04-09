a98c89229fcbfca703c9c684e1b492c7de8abdbd
BawerMY <bawermy123@gmail.com>
2023-04-09 04:03:56 +0200

    save

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://forlens.netlify.app/api/api/';
+const baseURL = 'http://bawermy.pythonanywhere.com//api/';
 
 const axiosInstance = axios.create({
 	baseURL: baseURL,

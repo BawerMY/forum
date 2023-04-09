dce775ec0b015c7a8c18a1a18c1e24eb745760b3
BawerMY <bawermy123@gmail.com>
2023-04-09 04:04:15 +0200

    arst

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://bawermy.pythonanywhere.com//api/';
+const baseURL = 'http://bawermy.pythonanywhere.com/api/';
 
 const axiosInstance = axios.create({
 	baseURL: baseURL,
